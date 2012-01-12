53b9d7b3fb053897a210207621405fb0736cec7a
Pierre-Yves Ritschard <pyr@spootnik.org>
2012-01-12 15:16:36 +0100

    simplify

--- src/clj_statsd.clj
+++ src/clj_statsd.clj
@@ -1,47 +1,54 @@
 (ns clj-statsd
+  "Send metrics to statsd."
   (:import [java.util Random])
   (:import [java.net DatagramPacket DatagramSocket InetAddress]))
 
-;; global defines
-(def agt        (agent nil))
-(def cfg        {})
-(def setup-done false)
+(def
+  ^{:doc "Atom holding the socket configuration"}
+  cfg
+  (atom nil))
 
-;; 
+(def
+  ^{:doc "Atom holding the datagram socket"}
+  sockagt
+  (agent nil))
+
 (defn setup
-  "initialize configuration"
+  "Initialize configuration"
   [host port]
-  (def cfg (assoc cfg
-                  :random (Random.)
-                  :host   (InetAddress/getByName host)
-                  :port   port
-                  :sock   (DatagramSocket.)))
-  (send agt (fn [_] (cfg :sock)))
-  (def setup-done true))
+  (send sockagt #(or % (DatagramSocket.)))
+  (swap! cfg #(or % {:random (Random.)
+                     :host   (InetAddress/getByName host)
+                     :port   port})))
 
 (defn publish
-  "send a metric over the network"
+  "Send a metric over the network. This should be a fully formatted
+   statsd metric line."
   [content rate]
-  (let [data  (.getBytes content)
-        len   (.length (into [] data))
-        pkt   (DatagramPacket. data len (cfg :host) (cfg :port))
-        f     (fn [sock pkt] (try (.send sock pkt) (catch Exception e nil)) sock)
-        spl?  (< rate 1.0)
-        match (<= (.nextDouble (cfg :random)) rate)
-        send? (or (not spl?) match)
-        fmt   (if spl? (format "|@%f" rate) "")]
-    (if send? (send agt f pkt))))
+  (when (or (>= rate 1.0) (<= (.nextDouble (:random @cfg)) rate))
+    (send sockagt
+          #(doto %1 (.send %2))
+          (DatagramPacket.
+            (.getBytes content)
+            (count content)
+            (:host @cfg)
+            (:port @cfg)))))
 
 (defn increment
+  "Increment a counter at specified rate, defaults to a one increment
+  with a 1.0 rate"
   ([k]        (increment k 1 1.0))
   ([k v]      (increment k v 1.0))
   ([k v rate] (publish (format "%s:%d|c" (name k) v) rate)))
 
 (defn timing
+  "Time an event at specified rate, defaults to 1.0 rate"
   ([k v]      (timing k v 1.0))
   ([k v rate] (publish (format "%s:%d|ms" (name k) v) rate)))
 
 (defn decrement
+  "Decrement a counter at specified rate, defaults to a one decrement
+  with a 1.0 rate"
   ([k]        (increment k -1 1.0))
   ([k v]      (increment k (* -1 v) 1.0))
   ([k v rate] (increment k (* -1 v) rate)))
