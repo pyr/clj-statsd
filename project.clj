590c8c733561365aea6775ea7ce0ccba161d55a9
Pierre-Yves Ritschard <pyr@spootnik.org>
2012-01-12 15:16:13 +0100

    update to clojure-1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-statsd "0.3.0"
+(defproject clj-statsd "0.3.1"
   :description "statsd protocol client"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+  :url         "https://github.com/pyr/clj-statsd"
+  :dependencies [[org.clojure/clojure "1.3.0"]])
