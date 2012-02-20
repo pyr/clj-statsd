188e3bc2d3910af466fac7310909a2bec2e8a191
Pierre-Yves Ritschard <pyr@spootnik.org>
2012-02-20 21:33:38 +0100

    prepare for new release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-statsd "0.3.2"
+(defproject clj-statsd "0.3.3"
   :description "statsd protocol client"
   :url         "https://github.com/pyr/clj-statsd"
   :dependencies [[org.clojure/clojure "1.3.0"]])
