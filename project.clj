b6d5a9801199307c1fad4554c7f9a2ef4874648e
Pierre-Yves Ritschard <pyr@spootnik.org>
2012-10-29 09:35:19 +0100

    bump version and move to 1.4.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-statsd "0.3.4"
+(defproject clj-statsd "0.3.5"
   :description "statsd protocol client"
   :url         "https://github.com/pyr/clj-statsd"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]])
