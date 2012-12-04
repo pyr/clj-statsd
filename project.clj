e2ad6673d32710425235fac5e000f7d7d43fc897
Pierre-Yves Ritschard <pyr@spootnik.org>
2012-12-04 20:26:40 +0100

    bump for prefix support

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-statsd "0.3.7"
+(defproject clj-statsd "0.3.8"
   :description "statsd protocol client"
   :url         "https://github.com/pyr/clj-statsd"
   :dependencies [[org.clojure/clojure "1.4.0"]])
