3515d8bb3305af4b2960cec9f366518f3db6d268
David Smith <shmish111@gmail.com>
2016-04-06 17:14:43 +0100

    namespace it to me

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-statsd "0.3.11"
+(defproject shmish111/clj-statsd "0.3.11"
   :description "statsd protocol client"
-  :url         "https://github.com/pyr/clj-statsd"
+  :url         "https://github.com/shmish111/clj-statsd"
   :dependencies [[org.clojure/clojure "1.4.0"]])
