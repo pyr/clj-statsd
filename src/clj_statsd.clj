c154fdef7cd58b755fe719f534a7d77dead0ba68
Ido Barkan <ido@appsflyer.com>
2018-11-11 12:57:54 +0200

    optimze formatting for high reporting rates

--- src/clj_statsd.clj
+++ src/clj_statsd.clj
@@ -54,20 +54,32 @@
                           nil))]
       (send sockagt send-packet packet))))
 
+(defn- with-rate [fmt]
+  (str fmt "|@%f"))
+
+(def increment-fmt "%s:%s|c")
+(def increment-fmt-rate (with-rate increment-fmt))
+(def timing-fmt "%s:%d|ms")
+(def timing-fmt-rate (with-rate timing-fmt))
+(def gauge-fmt "%s:%s|g")
+(def gauge-fmt-rate (with-rate gauge-fmt))
+(def unique-fmt "%s:%d|s")
+(def unique-fmt-rate (with-rate unique-fmt))
+
 (defn publish
   "Send a metric over the network, based on the provided sampling rate.
   This should be a fully formatted statsd metric line."
 
-  [^String content rate tags]
+  [^String fmt ^String fmt-rate k v rate tags]
   (cond
     (nil? @cfg)
     nil
 
     (>= rate 1.0)
-    (send-stat (:prefix @cfg) content tags)
+    (send-stat (:prefix @cfg) (format fmt (name k) v) tags)
 
     (<= (.nextDouble ^Random (:random @cfg)) rate)
-    (send-stat (:prefix @cfg) (format "%s|@%f" content rate) tags)
+    (send-stat (:prefix @cfg) (format fmt-rate (name k) v rate) tags)
 
     :else
     nil))
@@ -78,13 +90,13 @@
   ([k]             (increment k 1 1.0 []))
   ([k v]           (increment k v 1.0 []))
   ([k v rate]      (increment k v rate []))
-  ([k v rate tags] (publish (format "%s:%s|c" (name k) v) rate tags)))
+  ([k v rate tags] (publish increment-fmt increment-fmt-rate k v rate tags)))
 
 (defn timing
   "Time an event at specified rate, defaults to 1.0 rate"
   ([k v]           (timing k v 1.0))
   ([k v rate]      (timing k v rate []))
-  ([k v rate tags] (publish (format "%s:%d|ms" (name k) v) rate tags)))
+  ([k v rate tags] (publish timing-fmt timing-fmt-rate k v rate tags)))
 
 (defn decrement
   "Decrement a counter at specified rate, defaults to a one decrement
@@ -98,14 +110,14 @@
   "Send an arbitrary value."
   ([k v]           (gauge k v 1.0 []))
   ([k v rate]      (gauge k v rate []))
-  ([k v rate tags] (publish (format "%s:%s|g" (name k) v) rate tags)))
+  ([k v rate tags] (publish gauge-fmt gauge-fmt-rate k v rate tags)))
 
 (defn unique
   "Send an event, unique occurences of which per flush interval
    will be counted by the statsd server. We have no rate call
    signature here because that wouldn't make much sense."
-  ([k v]      (publish (format "%s:%d|s" (name k) v) 1.0 []))
-  ([k v tags] (publish (format "%s:%d|s" (name k) v) 1.0 tags)))
+  ([k v]      (publish unique-fmt unique-fmt-rate k v 1.0 []))
+  ([k v tags] (publish unique-fmt unique-fmt-rate k v 1.0 tags)))
 
 (defmacro with-sampled-timing
   "Time the execution of the provided code, with sampling."
