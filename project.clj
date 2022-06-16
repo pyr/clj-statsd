58c013bd47e5e8f2c4c157af060f48689c47af31
Pierre-Yves Ritschard <pyr@spootnik.org>
2022-06-16 19:42:10 +0200

    Version 0.4.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-statsd "0.4.1-SNAPSHOT"
+(defproject clj-statsd "0.4.1"
   :description "statsd protocol client"
   :url         "https://github.com/shmish111/clj-statsd"
   :license {:name "MIT License"
