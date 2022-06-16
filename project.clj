35a3c916c012d297a8c0b2d2159f3b1032105058
Pierre-Yves Ritschard <pyr@spootnik.org>
2022-06-16 19:38:25 +0200

    project: add tests via github actions

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url         "https://github.com/shmish111/clj-statsd"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
+  :deploy-repositories [["releases" :clojars]
+                        ["snapshots" :clojars]]
+  :profiles {:dev {:global-vars    {*warn-on-reflection* true}}}  
   :dependencies [[org.clojure/clojure "1.8.0"]])
