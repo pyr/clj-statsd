156c3efaff520dfeb334cb72999626c269e4420e
Pierre-Yves Ritschard <pyr@spootnik.org>
2022-06-16 21:40:40 +0200

    switch to deps.edn with tools.project

--- project.clj
+++
@@ -1,9 +0,0 @@
-(defproject clj-statsd "0.4.2-SNAPSHOT"
-  :description "statsd protocol client"
-  :url         "https://github.com/shmish111/clj-statsd"
-  :license {:name "MIT License"
-            :url "https://opensource.org/licenses/MIT"}
-  :deploy-repositories [["releases" :clojars]
-                        ["snapshots" :clojars]]
-  :profiles {:dev {:global-vars    {*warn-on-reflection* true}}}  
-  :dependencies [[org.clojure/clojure "1.8.0"]])
